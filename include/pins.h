9626df15c92dd1c40f908d4755aca15111da7d34
Karol Orszulik <orkarox@gmail.com>
2024-11-02 19:38:48 +0100

    Definicje pinów do 2-silnikowego modelu

---
+++ include/pins.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#pragma region Motors
+
+    #define MOTOR_1A    12
+    #define MOTOR_1B    13
+    #define ENCODER_1A  15
+    #define ENCODER_1B  4
+
+    #define MOTOR_2A    16
+    #define MOTOR_2B    17
+    #define ENCODER_2A  5
+    #define ENCODER_2B  23
+
+    // #define MOTOR_3A
+    // #define MOTOR_3B
+    // #define ENCODER_3A
+    // #define ENCODER_3B
+
+    // #define MOTOR_4A
+    // #define MOTOR_4B
+    // #define ENCODER_4A
+    // #define ENCODER_4B
+
+#pragma endregion
+
+
+#pragma region HX711
+
+    #define HX711_1SCK  32
+    #define HX711_1DT   33
+
+    #define HX711_2SCK  25
+    #define HX711_2DT   26
+
+#pragma endregion
+
+
+#pragma region IMU
+    
+        #define IMU_SDA 21
+        #define IMU_SCL 22
+
+#pragma endregion
