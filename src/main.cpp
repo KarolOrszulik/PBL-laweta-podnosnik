594a981df98289523a1ae1e0dfdd1dc91df5d86b
Karol Orszulik <orkarox@gmail.com>
2024-11-03 19:42:58 +0100

    All demos working

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 #include "Motor.h"
 #include "HX711.h"
-// #include "DFRobot_BMI160.h"
+#include "DFRobot_BMI160.h"
 
 Motor motor1(MOTOR_1A, MOTOR_1B, ENCODER_1A, ENCODER_1B);
 Motor motor2(MOTOR_2A, MOTOR_2B, ENCODER_2A, ENCODER_2B);
@@ -11,15 +11,15 @@ Motor motor2(MOTOR_2A, MOTOR_2B, ENCODER_2A, ENCODER_2B);
 static HX711 hx711_1;
 static HX711 hx711_2;
 
-// static DFRobot_BMI160 bmi160;
-// constexpr uint8_t BMI160_ADDR = 0x68;
+static DFRobot_BMI160 bmi160;
+constexpr uint8_t BMI160_ADDR = 0x69;
 
 
 void setup()
 {
     // set up serial
     Serial.begin(115200);
-    delay(500);
+    delay(1000);
 
     motor1.setPulsesPerMillimeter(695);
     motor2.setPulsesPerMillimeter(695);
@@ -34,15 +34,15 @@ void setup()
     hx711_2.begin(HX711_2DT, HX711_2SCK);
 
     // set up IMU
-    // if (bmi160.I2cInit(BMI160_ADDR) == BMI160_OK)
-    // {
-    //     Serial.println("BMI160 found");
-    // }
-    // else
-    // {
-    //     Serial.println("BMI160 not found");
-    //     while(1); // hang program
-    // }
+    if (bmi160.I2cInit(BMI160_ADDR) == BMI160_OK)
+    {
+        Serial.println("BMI160 found");
+    }
+    else
+    {
+        Serial.println("BMI160 not found");
+        while(1); // hang program
+    }
 
     Serial.println("Setup complete");
 }
@@ -52,7 +52,7 @@ void motorEncoderDemo()
     Serial.println("Running motor/encoder demo");
     
     motor1.move(2);
-    motor2.move(-3);
+    motor2.move(3);
     while (!motor1.isInRange() || !motor2.isInRange())
     {
         motor1.update();
@@ -61,7 +61,6 @@ void motorEncoderDemo()
 
     Serial.println("Motors in range");
     delay(1000);
-
 }
 
 void hx711Demo()
@@ -75,39 +74,68 @@ void hx711Demo()
     delay(100);
 }
 
-// void bmi160Demo()
-// {
-//     constexpr uint32_t DEMO_TIME = 5000;
-//     uint32_t timeStart = millis();
+void bmi160Demo()
+{
 
-//     Serial.println("Running BMI160 demo");
-//     while (millis() - timeStart < DEMO_TIME)
-//     {
-//         int16_t accelGyro[6];
+    int16_t accelGyro[6];
 
-//         int ret = bmi160.getAccelGyroData(accelGyro);
+    int ret = bmi160.getAccelGyroData(accelGyro);
 
-//         if (ret != 0)
-//         {
-//             Serial.println("Error getting BMI160 data");
-//             return;
-//         }
+    if (ret != 0)
+    {
+        Serial.println("Error getting BMI160 data");
+        return;
+    }
 
-//         Serial.println("Accel X: " + String(accelGyro[0]));
-//         Serial.println("Accel Y: " + String(accelGyro[1]));
-//         Serial.println("Accel Z: " + String(accelGyro[2]));
-//         Serial.println("Gyro X: " + String(accelGyro[3]));
-//         Serial.println("Gyro Y: " + String(accelGyro[4]));
-//         Serial.println("Gyro Z: " + String(accelGyro[5]));
-//     }
-// }
+    Serial.println("Accel: " + String(accelGyro[0]) + " " + String(accelGyro[1]) + " " + String(accelGyro[2])
+               + "\tGyro " + String(accelGyro[3]) + " " + String(accelGyro[4]) + " " + String(accelGyro[5]));
+
+    delay(100);
+}
 
 void loop()
 {
-    Serial.println("Running demo");
-    // motorEncoderDemo();
-    hx711Demo();
-    // bmi160Demo();
+    static enum
+    {
+        NONE,
+        MOTOR_ENCODER,
+        HX711,
+        BMI160
+    } state = NONE;
 
-    delay(2000);
+
+    if (Serial.available())
+    {
+        char c = Serial.read();
+        switch (c)
+        {
+            case 'm':
+                state = MOTOR_ENCODER;
+                break;
+            case 'h':
+                state = HX711;
+                break;
+            case 'b':
+                state = BMI160;
+                break;
+            default:
+                state = NONE;
+                break;
+        }
+    }
+
+    switch (state)
+    {
+        case MOTOR_ENCODER:
+            motorEncoderDemo();
+            break;
+        case HX711:
+            hx711Demo();
+            break;
+        case BMI160:
+            bmi160Demo();
+            break;
+        default:
+            break;
+    }
 }
